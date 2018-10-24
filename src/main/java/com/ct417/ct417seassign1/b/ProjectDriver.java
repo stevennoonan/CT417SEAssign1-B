7384685c3e1960463c401aa43117c948d579288e
Steven Noonan <stevennoonan013@gmail.com>
2018-10-24 17:03:51 +0100

    Formatting for the output

--- src/main/java/com/ct417/ct417seassign1/b/ProjectDriver.java
+++ src/main/java/com/ct417/ct417seassign1/b/ProjectDriver.java
@@ -19,12 +19,12 @@ public class ProjectDriver {
         //Creating fictional students
         Student stu1 = new Student("Shawn Carter", new LocalDate(1969, 12, 4), 10001);
         Student stu2 = new Student("Trae Young", new LocalDate(1998, 9, 19), 10002);
-        Student stu3 = new Student("Malcolm Miller", new LocalDate(1992, 1, 19), 10003);
+        Student stu3 = new Student("Malcom Miller", new LocalDate(1992, 1, 19), 10003);
         Student stu4 = new Student("Kevin Gates", new LocalDate(1986, 2, 5), 10004);
         Student stu5 = new Student("Gary Garcia", new LocalDate(2000, 8, 17), 10005);
         Student stu6 = new Student("Gustav Elijah", new LocalDate(1996, 11, 1), 10006);
         Student stu7 = new Student("Radric Davis", new LocalDate(1980, 2, 12), 10007);
-        Student stu8 = new Student("Miles McCollum", new LocalDate(1997, 8, 23), 10008);
+        Student stu8 = new Student("Miles McColum", new LocalDate(1997, 8, 23), 10008);
         
         //Adding students to a list
         ArrayList<Student> studentList = new ArrayList<>();
@@ -86,22 +86,22 @@ public class ProjectDriver {
         csModList.forEach((m) -> {
             System.out.println("\n" + m.getModName() + "\t" + m.getModId());
             System.out.println("\tStudents Registered for Module:");
-            System.out.println("\n\t\tStudent Name" + "\t" +"Username"+ "\t" +"Date Of Birth"+
-                "\t" +"Age"+ "\t" +"ID");
+            System.out.println("\n\t\tStudent Name" + "\t\t" +"Username"+ "\t\t" +"Date Of Birth"+
+                "\t\t" +"Age"+ "\t\t" +"ID");
             List<Student> sList = m.getStudents();
             sList.forEach((s) -> {
-                System.out.println("\t\t" + s.getName()  + "\t" + s.getUsername()  + "\t" + s.getDateOfBirth()
-                        + "\t" + s.getAge()  + "\t" + s.getId() + "\t");
+                System.out.println("\t\t" + s.getName()  + "\t\t" + s.getUsername()  + "\t\t" + s.getDateOfBirth()
+                        + "\t\t" + s.getAge()  + "\t\t" + s.getId() + "\t");
             });
         });
         //Getting Students assigned to the course and printing to the console
         List<Student> csStuList = cs.getStudentsList();
         System.out.println("\n\nStudents registered for course: ");
-        System.out.println("\nStudent Name" + "\t" +"Username"+ "\t" +"Date Of Birth"+
-                "\t" +"Age"+ "\t" +"ID");
+        System.out.println("\nStudent Name" + "\t\t" +"Username"+ "\t\t" +"Date Of Birth"+
+                "\t\t" +"Age"+ "\t\t" +"ID");
         csStuList.forEach( (s) -> {
-        System.out.println(s.getName()  + "\t" + s.getUsername()  + "\t" + s.getDateOfBirth()
-                 + "\t" + s.getAge()  + "\t" + s.getId() + "\t");
+        System.out.println(s.getName()  + "\t\t" + s.getUsername()  + "\t\t" + s.getDateOfBirth()
+                 + "\t\t" + s.getAge()  + "\t\t" + s.getId() + "\t\t");
     });
     }
 }
